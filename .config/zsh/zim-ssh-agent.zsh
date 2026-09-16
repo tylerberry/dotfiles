83c2ba79dd65fa6b036766d9f2f9a958a0ffe763
Tyler Berry <tyler@3james.com>
2026-09-15 19:45:26 -0600

    Use customized zim ssh module to allow better XDG location compliance.

---
+++ .config/zsh/zim-ssh-agent.zsh
@@ -0,0 +1,38 @@
+# This is a mild edit of https://github.com/zimfw/ssh/blob/master/init.zsh.
+#
+# It's almost identical except that it makes the location for the stored agent
+# connection configurable rather than hardcoding it to ~/.ssh-agent.
+
+# Don't do anything unless we can actually use ssh-agent
+(( ${+commands[ssh-agent]} )) && () {
+  ssh-add -l &>/dev/null
+  if (( ? == 2 )); then
+    # Unable to contact the authentication agent
+
+    # Load stored agent connection info
+    local ssh_env
+    zstyle -s ':zim:ssh' agent_file 'ssh_env'
+    [[ -z $ssh_env ]] && ssh_env=${HOME}/.ssh-agent
+
+    if [[ -r ${ssh_env} ]] source ${ssh_env} >/dev/null
+
+    ssh-add -l &>/dev/null
+    if (( ? == 2 )); then
+        # Start agent and store agent connection info
+        (umask 066; ssh-agent >! ${ssh_env})
+        source ${ssh_env} >/dev/null
+    fi
+  fi
+
+  # Load identities
+  ssh-add -l &>/dev/null
+  if (( ? == 1 )); then
+    local -a zssh_ids
+    zstyle -a ':zim:ssh' ids 'zssh_ids'
+    if (( ${#zssh_ids} )); then
+      ssh-add ${HOME}/.ssh/${^zssh_ids} 2>/dev/null
+    else
+      ssh-add 2>/dev/null
+    fi
+  fi
+}
